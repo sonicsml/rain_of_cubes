3ed810899d9b8bf5efd713cb69d51b1e9987b533
Dmitry Polovkov <sonics.scs@gmail.com>
2025-03-14 13:22:37 +0300

    Add: init

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField] private int _minLifeTime = 2;
+    [SerializeField] private int _maxLifeTime = 5;
+
+    Color defaultColor = Color.white;
+    Color collidedColor = Color.red;
+
+    private Renderer _cubeRenderer;
+
+    private bool _hasCollided = false;
+
+    private int _lifeTime;
+
+    private Action<Cube> _destroyAction;
+
+    private void Start()
+    {
+        _cubeRenderer = GetComponent<Renderer>();
+        _lifeTime = Random.Range(_minLifeTime, _maxLifeTime);
+    }
+
+    public void Init(Action<Cube> destroyAction)
+    {
+        _destroyAction = destroyAction;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            _hasCollided = true;
+         
+            if (_hasCollided)
+            {
+                StartCoroutine(DestroyAfterDelay());
+                _cubeRenderer.material.color = collidedColor;
+            }
+        }
+    }
+
+    private IEnumerator DestroyAfterDelay()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+
+        _destroyAction?.Invoke(this);
+    }
+    public void ResetCube()
+    {
+        _hasCollided = false;
+        _cubeRenderer.material.color = defaultColor;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Cube _prefab;
+    [SerializeField] private int _spawnAmout = 3;
+    [SerializeField] private bool _usePool;
+    [SerializeField] private int _defaultCapacity = 10;
+    [SerializeField] private int _maxCapacity = 50;
+
+    private ObjectPool<Cube> _pool;
+
+    private void Start()
+    {
+        _pool = new ObjectPool<Cube>(() =>
+        {
+            return Instantiate(_prefab);
+        }, cube =>
+        {
+            cube.gameObject.SetActive(true);
+        }, cube =>
+        {
+            cube.gameObject.SetActive(false);
+        }, cube =>
+        {
+            Destroy(cube.gameObject);
+        }, false, _defaultCapacity, _maxCapacity);
+
+        InvokeRepeating(nameof(Create), 0.2f, 0.2f);
+    }
+
+    private void Create()
+    {
+        for (var i = 0; i < _spawnAmout; i++)
+        {
+            var cube = _usePool ? _pool.Get() : Instantiate(_prefab);
+            cube.transform.position = transform.position + Random.insideUnitSphere * 7;
+            cube.Init(DestroyCube);
+        }
+    }
+
+    private void DestroyCube(Cube cube)
+    {
+        if (_usePool)
+        {
+            cube.ResetCube();
+            _pool.Release(cube);
+        }
+        else Destroy(cube.gameObject);
+    }
+}
